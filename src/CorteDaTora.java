b14fcf63960ac5867379e582c2accf6dd13fb9a5
Pedro Carvalhaes <pedrohwc1998@gmail.com>
2023-10-20 19:06:23 -0400

    adição de graficos

--- src/CorteDaTora.java
+++ src/CorteDaTora.java
@@ -5,10 +5,72 @@ Ralson Rwan dos Santos Lima
 
 import de.vandermeer.asciitable.AsciiTable;
 import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class CorteDaTora {
+
+    private static void plotarGraficos(
+            ArrayList<Integer> ns,
+            ArrayList<Integer> valoresPD,
+            ArrayList<Integer> valoresGreedy,
+            ArrayList<Double> temposPD,
+            ArrayList<Double> temposGreedy
+    ) {
+
+        // Convertendo ArrayList<Integer> para double[]
+        double[] ns_double = ns.stream()
+                .mapToDouble(Integer::doubleValue)
+                .toArray();
+
+        double[] valoresPD_double = valoresPD.stream()
+                .mapToDouble(Integer::doubleValue)
+                .toArray();
+        double[] valoresGreedy_double = valoresGreedy.stream()
+                .mapToDouble(Integer::doubleValue)
+                .toArray();
+        double[][] matriz1 = new double[][]{
+                valoresPD_double,
+                valoresGreedy_double
+        };
+
+        // Gráfico para Valor Total de Venda
+        XYChart chart1 = QuickChart.getChart(
+                "DP vs. Greedy",
+                "n",
+                "Valor total de venda",
+                new String[]{"DP", "Greedy"},
+                ns_double,  // Usando ns_double aqui
+                matriz1
+        );
+        new SwingWrapper<>(chart1).displayChart();
+
+        // Gráfico para Tempo de Processamento
+        double[] temposPD_double = temposPD.stream()
+                .mapToDouble(Double::doubleValue)
+                .toArray();
+        double[] temposGreedy_double = temposGreedy.stream()
+                .mapToDouble(Double::doubleValue)
+                .toArray();
+        double[][] matriz2 = new double[][]{
+                temposPD_double,
+                temposGreedy_double
+        };
+
+        XYChart chart2 = QuickChart.getChart(
+                "DP vs. Greedy",
+                "n",
+                "Tempo (ms)",
+                new String[]{"DP", "Greedy"},
+                ns_double,  // Usando ns_double aqui
+                matriz2
+        );
+        new SwingWrapper<>(chart2).displayChart();
+    }
 
     public static int receitaMaximaPD(int[] precos, int comprimento) {
         int[] dp = new int[comprimento + 1];
@@ -47,12 +109,18 @@ public class CorteDaTora {
     }
 
     public static void main(String[] args) {
-        int inc = 100;
-        int fim = 2000;
-        int stp = 100;
+        int inc = 1000;
+        int fim = 20000;
+        int stp = 1000;
 
         Random rand = new Random();
         AsciiTable at = new AsciiTable();
+
+        ArrayList<Integer>  ns              = new ArrayList<>();
+        ArrayList<Integer>  valoresPD       = new ArrayList<>();
+        ArrayList<Integer>  valoresGreedy   = new ArrayList<>();
+        ArrayList<Double>   temposPD        = new ArrayList<>();
+        ArrayList<Double>   temposGreedy    = new ArrayList<>();
 
         at.addRule();
         at.addRow("n", "vDP", "tDP", "vGreedy", "tGreedy", "%").setTextAlignment(TextAlignment.CENTER);
@@ -85,10 +153,19 @@ public class CorteDaTora {
                     String.format("%.6f", tempoGuloso),
                     String.format("%.2f", porcentagemAcerto)
             ).setTextAlignment(TextAlignment.CENTER);
+
+
+            ns.add(n);
+            valoresPD.add(receitaPD);
+            valoresGreedy.add(receitaGuloso);
+            temposPD.add(tempoPD);
+            temposGreedy.add(tempoGuloso);
         }
         at.addRule();
 
         String rend = at.render();
         System.out.println(rend);
+
+        plotarGraficos(ns, valoresPD, valoresGreedy, temposPD, temposGreedy);
     }
 }
